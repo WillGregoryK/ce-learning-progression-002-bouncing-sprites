a784073bdde63994fb26375a7beacf819c19c173
WillGregoryK <69864579+WillGregoryK@users.noreply.github.com>
2020-09-28 01:13:05 -0600

    Create microbit-program-1-2-2.js

---
+++ programs/microbit-program-1-2-2.js
@@ -0,0 +1,14 @@
+for(let i = 4; i > 3; i++) {
+    led.plot(0, 0)
+    basic.pause(35)
+    basic.clearScreen()
+    led.plot(1, 1)
+    basic.pause(35)
+    basic.clearScreen()
+    led.plot(2, 2)
+    basic.pause(35)
+    basic.clearScreen()
+    led.plot(3, 3)
+    basic.pause(35)
+    basic.clearScreen()
+    led.plot(4, 4)}
