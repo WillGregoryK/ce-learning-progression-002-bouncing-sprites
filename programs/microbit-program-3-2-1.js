8b2ee3a339560e9789dcf2514c66d40fb441d84f
WillGregoryK <69864579+WillGregoryK@users.noreply.github.com>
2020-09-29 22:23:11 -0600

    Create microbit-program-3-2-1.js

---
+++ programs/microbit-program-3-2-1.js
@@ -0,0 +1,6 @@
+let favoriteIcons : IconNames[] = [IconNames.Heart, IconNames.Cow, IconNames.Diamond, IconNames.SmallHeart, IconNames.Ghost]
+
+for (let i=0; i < favoriteIcons.length; i ++) {     // note the use of the length property
+    basic.showIcon(favoriteIcons[i])
+    basic.pause(100)
+}
